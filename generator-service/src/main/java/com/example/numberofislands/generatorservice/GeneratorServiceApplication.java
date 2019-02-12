44dc3710b06cc87d8ac68479e725a47c21c82ccc
Rustam Karimov <rkarimov.r@gmail.com>
2019-02-12 23:42:17 +0400

    Generator Service initial commit

---
+++ generator-service/src/main/java/com/example/numberofislands/generatorservice/GeneratorServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.numberofislands.generatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorServiceApplication.class, args);
+    }
+
+}
+
